aa915584c2c78263a79eba5f71a37b1006ffb61f
Itchy <itchy@laurinmurer.ch>
2015-12-12 20:25:02 +0100

    Changed html-files to scala.html-files

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -7,8 +7,10 @@ import play.data.Form;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
+import play.twirl.api.Html;
 
 import javax.inject.Inject;
+import java.lang.reflect.InvocationTargetException;
 
 public class Application extends Controller {
 
@@ -36,6 +38,19 @@ public class Application extends Controller {
         }
         response().setContentType("text/calendar");
         return ok(calendarMapper.map(calculation.calculate(form.get())));
+    }
+
+    public Result index() {
+        return ok(views.html.index.render());
+    }
+
+    public Result read(String scalaHtmlFile) {
+        try {
+            final Class<?> template = this.getClass().getClassLoader().loadClass("views.html.templates." + scalaHtmlFile);
+            return ok((Html) template.getMethod("render").invoke(template));
+        } catch (IllegalAccessException | InvocationTargetException | NoSuchMethodException | ClassNotFoundException e) {
+            return ok(views.html.notFound.render());
+        }
     }
 
 }
