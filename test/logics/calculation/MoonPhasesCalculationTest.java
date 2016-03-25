774f081a14e1cff5e829bd46665971278ed1294a
Itchy <itchy@laurinmurer.ch>
2016-03-25 12:42:05 +0100

    Fixed tests and a bug in the SimpleAstronomyLib with leap years

--- test/logics/calculation/MoonPhasesCalculationTest.java
+++ test/logics/calculation/MoonPhasesCalculationTest.java
@@ -7,6 +7,9 @@ import org.hamcrest.FeatureMatcher;
 import org.hamcrest.Matcher;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import play.i18n.Lang;
 import play.i18n.MessagesApi;
 import play.test.WithApplication;
 
@@ -15,18 +18,28 @@ import java.time.ZoneOffset;
 import java.time.ZonedDateTime;
 import java.util.Collection;
 import java.util.TreeSet;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class MoonPhasesCalculationTest extends WithApplication {
 
-    private MoonPhasesCalculation cut = new MoonPhasesCalculation(mock(MessagesApi.class));
+    private MoonPhasesCalculation cut;
     private RequestForm requestForm;
 
     @Before
     public void setup() {
+        final MessagesApi messagesApi = mock(MessagesApi.class);
+        when(messagesApi.get(any(Lang.class), anyString(), any(Object.class))).thenAnswer(invocation -> {
+            final Object[] arguments = invocation.getArguments();
+            return arguments[arguments.length - 1];
+        });
+        cut = new MoonPhasesCalculation(messagesApi);
         requestForm = new RequestForm();
         requestForm.getPhases().put(MoonPhaseType.FULLMOON, true);
         requestForm.getPhases().put(MoonPhaseType.NEWMOON, true);
@@ -87,6 +100,18 @@ public class MoonPhasesCalculationTest extends WithApplication {
         final Collection<ZonedEvent> actual = calculate(requestForm);
 
         assertThat(actual, hasSize(42));
+    }
+
+    @Test
+    public void testEventOnLapYear() {
+        requestForm.getPhases().clear();
+        requestForm.getPhases().put(MoonPhaseType.DAILY, true);
+        requestForm.setFrom(ZonedDateTime.of(2016, 2, 29, 12, 0, 0, 0, ZoneOffset.UTC));
+        requestForm.setTo(ZonedDateTime.of(2016, 3, 1, 12, 0, 0, 0, ZoneOffset.UTC));
+
+        final Collection<ZonedEvent> actual = calculate(requestForm);
+
+        assertThat(actual.stream().map(ZonedEvent::getTitle).collect(Collectors.toList()), containsInAnyOrder("55", "64"));
     }
 
     private Matcher<ZonedEvent> eventAt(final LocalDate expectedDate) {
