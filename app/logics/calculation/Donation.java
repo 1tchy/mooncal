bf33ad38321b305871b643b1b6432238d8f70681
Itchy <itchy@laurinmurer.ch>
2024-02-10 20:48:09 +0100

    Added "Buy me a coffee" link to iCal files

---
+++ app/logics/calculation/Donation.java
@@ -0,0 +1,8 @@
+package logics.calculation;
+
+public class Donation {
+    public static final String BUY_ME_A_COFFEE_LINK = "https://www.buymeacoffee.com/1tchy";
+
+    private Donation() {
+    }
+}
