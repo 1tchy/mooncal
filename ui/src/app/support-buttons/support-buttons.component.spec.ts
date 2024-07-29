518fdd32e2844aa5f5968db28c207f3e49e43117
Itchy <itchy@laurinmurer.ch>
2024-07-29 17:55:00 +0200

    Fixed broken UI test

--- ui/src/app/support-buttons/support-buttons.component.spec.ts
+++ ui/src/app/support-buttons/support-buttons.component.spec.ts
@@ -1,14 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SupportButtonsComponent } from './support-buttons.component';
+import {ActivatedRoute} from "@angular/router";
+import {of} from "rxjs";
+import messages from "../messages.en.json";
 
 describe('SupportComponent', () => {
   let component: SupportButtonsComponent;
   let fixture: ComponentFixture<SupportButtonsComponent>;
 
   beforeEach(async () => {
+    const route = {data: {messages: messages}};
     await TestBed.configureTestingModule({
-      imports: [SupportButtonsComponent]
+      imports: [SupportButtonsComponent],
+      providers: [{
+        provide: ActivatedRoute,
+        useValue: {
+          snapshot: route,
+          data: of(route.data)
+        }
+      }]
     })
     .compileComponents();
 
